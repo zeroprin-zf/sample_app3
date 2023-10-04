b88328482a3fdf7f7a07805a3d37b019915939e5
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2023-10-04 01:45:08 +0000

    Routingまで

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,33 @@
 class ListsController < ApplicationController
-  def new
-  end
+  
 
+    #Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する
+  def new
+    @list = List.new
+  end
+  
+  def create
+    #　1.&2.　データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    #4.トップ画面へリダイレクト
+    redirect_to '/top'
+  end
+  
   def index
+    @lists = List.all
   end
 
   def show
   end
 
   def edit
+  end
+  
+  private
+  #ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'lists/new'gi
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
+  get 'lists/new'
+  #post 'lists' => 'lists#create'
+  post 'lists' => 'lists#create'
+  get 'lists' => 'lists#index'
   get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
